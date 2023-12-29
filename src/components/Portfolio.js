00751ddd519d6fd4931134f684cc4a1d98dad7ea
John Doe <chchch7002@gmail.com>
2023-12-29 12:35:57 +0100

    projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import corse from '../asists/corse.svg';
 
 export default function Portfolio() {
 
@@ -9,6 +9,30 @@ export default function Portfolio() {
         QUALITY WORK<br />
         RECENTLY DONE PROJECT
         </h2>
+        <div className='projects-container'>
+          <a href='#' className='project-card-container'>
+            <div className='project-card-img-container'>
+              <img className='project-card-img' src='https://preview.colorlib.com/theme/satner/img/portfolio/p1.jpg' />
+              <div className='overlay'>
+              </div>
+                <img className='overlay-img' src={corse} />
+            </div>
+            <p className='project-card-title'>MINIMAL DESIGN</p>
+            <p className='project-card-text'>Animated, portfolio</p>
+            
+          </a>
+          <a href='#' className='project-card-container'>
+            <div className='project-card-img-container'>
+              <img className='project-card-img' src='https://preview.colorlib.com/theme/satner/img/portfolio/p2.jpg' />
+              <div className='overlay'>
+              </div>
+                <img className='overlay-img' src={corse} />
+            </div>
+            <p className='project-card-title'>MINIMAL DESIGN</p>
+            <p className='project-card-text'>Animated, portfolio</p>
+            
+          </a>
+        </div>
     </section>
   )
 }
--- src/data/Services-data.js
+++ src/data/Services-data.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import webDev from "../asists/web dev.svg";
 
 export const ServicesData =[
